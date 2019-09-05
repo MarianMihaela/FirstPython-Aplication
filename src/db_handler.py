c96168f8fa8b4e57522d0d3e0cc5e1074fec7830
MarianMihaela <miha_92_sweet@yahoo.com>
2019-09-05 21:22:32 +0300

    interrogated the database "Angajat" table

---
+++ src/db_handler.py
@@ -0,0 +1,48 @@
+import sqlite3
+from sqlite3 import Error
+
+class AngajatDB(object):
+	def __init__(self, db_file):
+		""" create a database connection to the SQLite database
+			specified by the db_file
+		:param db_file: database file
+		:return: Connection object or None
+		"""
+		try:
+			self.conn = sqlite3.connect(db_file)
+		except Error as e:
+			print(e)
+
+ 
+	def select_all_tasks(self):
+		"""
+		Query all rows in the tasks table
+		:param conn: the Connection object
+		:return:
+		"""
+		cur = self.conn.cursor()
+		cur.execute("SELECT * FROM contacts")
+	
+		rows = cur.fetchall()
+	
+		for row in rows:
+			print(row)
+ 
+ 
+	def select_all_employees(self):
+		"""
+		Query tasks by priority
+		:param conn: the Connection object
+		:param priority:
+		:return:
+		"""
+		cur = self.conn.cursor()
+		#cur.execute("SELECT * FROM tasks WHERE priority=?", (priority,))
+		cur.execute("SELECT * FROM Angajat")
+	
+		rows = cur.fetchall()
+	
+		for row in rows:
+			print(row)
+ 
+ 
--- src/main.py
+++ src/main.py
@@ -1,1 +1,5 @@
-print("Working!!!")
+from db_handler import *
+
+a = AngajatDB("C:\Repos\FirstPython-Aplication\database\demodb.db")
+
+a.select_all_employees()
